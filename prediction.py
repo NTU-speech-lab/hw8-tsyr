6d1f970716c1adc1e146599d3cbf93a9e030b3d9
tsyr <33116430+tsyr@users.noreply.github.com>
2020-05-15 12:06:30 +0800

    Add files via upload

---
+++ prediction.py
@@ -0,0 +1,109 @@
+import torch
+import os
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.models as models
+
+def loss_fn_kd(outputs, labels, teacher_outputs, T=20, alpha=0.5):
+    # 一般的Cross Entropy
+    hard_loss = F.cross_entropy(outputs, labels) * (1. - alpha)
+    # 讓logits的log_softmax對目標機率(teacher的logits/T後softmax)做KL Divergence。
+    soft_loss = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(outputs/T, dim=1),
+                             F.softmax(teacher_outputs/T, dim=1)) * (alpha * T * T)
+    return hard_loss + soft_loss
+
+import re
+import torch
+from glob import glob
+from PIL import Image
+import torchvision.transforms as transforms
+
+class MyDataset(torch.utils.data.Dataset):
+
+    def __init__(self, folderName, transform=None):
+        self.transform = transform
+        self.data = []
+        self.label = []
+
+        for img_path in sorted(glob(folderName + '/*.jpg')):
+            try:
+                # Get classIdx by parsing image path
+                class_idx = int(re.findall(re.compile(r'\d+'), img_path)[1])
+            except:
+                # if inference mode (there's no answer), class_idx default 0
+                class_idx = 0
+
+            image = Image.open(img_path)
+            # Get File Descriptor
+            image_fp = image.fp
+            image.load()
+            # Close File Descriptor (or it'll reach OPEN_MAX)
+            image_fp.close()
+
+            self.data.append(image)
+            self.label.append(class_idx)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        image = self.data[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, self.label[idx]
+
+
+trainTransform = transforms.Compose([
+    transforms.RandomCrop(256, pad_if_needed=True, padding_mode='symmetric'),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+])
+testTransform = transforms.Compose([
+    transforms.CenterCrop(256),
+    transforms.ToTensor(),
+])
+
+def get_dataloader(mode='training', batch_size=32):
+
+    assert mode in ['training', 'testing', 'validation']
+
+    dataset = MyDataset(
+        f'./food-11/{mode}',
+        transform=trainTransform if mode == 'training' else testTransform)
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=(mode == 'training'))
+
+    return dataloader
+
+
+#######################################################
+from model import StudentNet
+model_dir = './student_model.bin'
+
+#model_best = torch.load('gdrive/My Drive/ml/hw3/models/model5.pth')
+model_best = torch.load(model_dir)
+
+test_loader = get_dataloader('testing', batch_size=32)
+
+model_best.eval()
+prediction = []
+with torch.no_grad():
+    for i, data in enumerate(test_loader):
+        test_pred = model_best(data.cuda())
+        test_label = np.argmax(test_pred.cpu().data.numpy(), axis=1)
+        for y in test_label:
+            prediction.append(y)
+
+#將結果寫入 csv 檔
+with open("./predict.csv", 'w') as f:
+    f.write('Id,Category\n')
+    for i, y in  enumerate(prediction):
+        f.write('{},{}\n'.format(i, y))
+
